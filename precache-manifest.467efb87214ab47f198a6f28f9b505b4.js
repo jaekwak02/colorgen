a7e37843783fb6e61d4cd7a5cba3e312802ccf4c
Jae Kwak <jaekwak02@gmail.com>
2020-03-09 00:40:52 -0500

    Updates

---
+++ precache-manifest.467efb87214ab47f198a6f28f9b505b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c77591418ee94bd874f39bcfd6750b",
+    "url": "/colorgen/index.html"
+  },
+  {
+    "revision": "b3bbd122b18b6208e64c",
+    "url": "/colorgen/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "6f9f16a9cab7ea4406c2",
+    "url": "/colorgen/static/css/main.49daa65a.chunk.css"
+  },
+  {
+    "revision": "b3bbd122b18b6208e64c",
+    "url": "/colorgen/static/js/2.0c2e6d7c.chunk.js"
+  },
+  {
+    "revision": "6f9f16a9cab7ea4406c2",
+    "url": "/colorgen/static/js/main.1724a69a.chunk.js"
+  },
+  {
+    "revision": "fbf1084a22fcdd348528",
+    "url": "/colorgen/static/js/runtime~main.d386e45a.js"
+  }
+]);
